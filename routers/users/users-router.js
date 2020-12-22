dc3ed22d5e0ef54f166ae96a3d5a73b3b68430a5
Evan Barton <notrabe1011@gmail.com>
2020-12-22 16:20:00 -0500

    added users model and router, still have to test and debug

---
+++ routers/users/users-router.js
@@ -0,0 +1,69 @@
+const router = require('express').Router()
+
+const Users = require('./users-model')
+
+router.get('/', (req, res) => {
+    Users.findAll()
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+    
+})
+
+router.get('/:id', validId, (req, res) => {
+    const {id} = req.params
+
+    Users.findById(id)
+        .then(user => {
+            res.status(200).json(user)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+router.put('/:id', validId, (req, res) => {
+    const {id} = req.params
+    const user = req.body
+
+    Users.editUser(user, id)
+        .then(user => {
+            res.status(200).json({message: 'User updated successfully'})
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+router.delete('/:id', validId, (req, res) => {
+    const {id} = req.params
+
+    Users.deleteUser(id)
+        .then(user => {
+            res.status(200).json({message: 'user succuessfully deleted'})
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+function validId (req, res, next) {
+    const {id}  = req.params
+
+    Users.findById(id) 
+        .then(user => {
+            if(user){
+                next()
+            } else { 
+                res.status(400).json({message: 'ID is invalid.'})
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+}
+
+module.exports = router
