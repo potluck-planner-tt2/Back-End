e59e6782a62cdabdb21fab05fae4d9668d7b327a
joeywire <61367542+joeywire@users.noreply.github.com>
2020-12-31 14:28:05 -0600

    attendance router built w/ get all endpoint working

--- api/server.js
+++ api/server.js
@@ -9,6 +9,7 @@ const authRouter = require('./routers/auth/auth-router');
 const usersRouter = require('./routers/users/users-router');
 const potluckRouter = require('./routers/potlucks/potlucks-router');
 const foodRouter = require('./routers/foods/foods-router');
+const attendanceRouter = require('./routers/attendance/attendance-router');
 const { restrictPath } = require('./middleware/auth-middleware');
 
 const server = express();
@@ -23,6 +24,7 @@ server.use('/api/auth', authRouter);
 server.use('/api/users', restrictPath, usersRouter);
 server.use('/api/potlucks', restrictPath, potluckRouter);
 server.use('/api/foods', restrictPath, foodRouter);
+server.use('/api/attendance',restrictPath, attendanceRouter);
 
 server.get('/', (req, res) => {
   res.status(200).json({ message: 'API up and running' });
