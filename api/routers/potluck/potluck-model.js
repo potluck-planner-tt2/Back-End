7512ae8c56b737c562deb6f0266def4f5ccea190
joeywire <61367542+joeywire@users.noreply.github.com>
2021-01-03 12:11:59 -0600

    get potluck attendees by ID added

--- api/routers/potluck/potluck-model.js
+++ api/routers/potluck/potluck-model.js
@@ -1,12 +1,13 @@
 const db = require('../../../data/dbConfig');
 
-const getAllAttendance = () => {
+const getAttendance = (id) => {
   return db('potluck_attendance as pa')
     .select('pa.id', 'p.name', 'u.username', 'pa.confirmed')
     .join('potlucks as p', { 'pa.pl_id': 'p.pl_id' })
-    .join('users as u', { 'pa.user_id': 'u.user_id' });
+    .join('users as u', { 'pa.user_id': 'u.user_id' })
+    .where('pa.pl_id', id);
 };
 
 module.exports = {
-  getAllAttendance,
+  getAttendance,
 };
--- api/routers/potluck/potluck-router.js
+++ api/routers/potluck/potluck-router.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
-const Attendance = require('./attendance-model');
+const Potluck = require('./potluck-model');
 
-router.get('/', async (req, res) => {
+router.get('/:id', async (req, res) => {
+  const { id } = req.params; 
+
   try {
-    const allAttendance = await Attendance.getAllAttendance();
+    const allAttendance = await Potluck.getAttendance(id);
     res.status(200).json(allAttendance);
   } catch (err) {
     res.status(500).json({ message: err.message });
