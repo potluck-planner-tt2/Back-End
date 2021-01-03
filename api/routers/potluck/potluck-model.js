5b9b04811620793dd429c3676d57a704b7353845
joeywire <61367542+joeywire@users.noreply.github.com>
2021-01-03 12:59:34 -0600

    added confirmation update

--- api/routers/potluck/potluck-model.js
+++ api/routers/potluck/potluck-model.js
@@ -8,6 +8,20 @@ const getAttendance = (id) => {
     .where('pa.pl_id', id);
 };
 
+const getUserAttendance = (id) => {
+  return db('potluck_attendance as pa')
+    .select('pa.id', 'p.name', 'u.username', 'pa.confirmed')
+    .join('potlucks as p', { 'pa.pl_id': 'p.pl_id' })
+    .join('users as u', { 'pa.user_id': 'u.user_id' })
+    .where('pa.user_id', id);
+};
+
+const editUserAttendance = (id, change) => {
+  return db('potluck_attendance').where({ id: id }).update(change);
+};
+
 module.exports = {
   getAttendance,
+  getUserAttendance,
+  editUserAttendance
 };
--- api/routers/potluck/potluck-router.js
+++ api/routers/potluck/potluck-router.js
@@ -2,11 +2,36 @@ const router = require('express').Router();
 const Potluck = require('./potluck-model');
 
 router.get('/:id', async (req, res) => {
-  const { id } = req.params; 
+  const { id } = req.params;
 
   try {
     const allAttendance = await Potluck.getAttendance(id);
     res.status(200).json(allAttendance);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.get('/user/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const userPotlucks = await Potluck.getUserAttendance(id);
+    res.status(200).json(userPotlucks);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.put('/confirmation/:id', async (req, res) => {
+  const { id } = req.params;
+  //Only need to pass in the 'attendance id' and the confirmation value
+  //if confirmation value is unchanged will throw an error stating such 
+  const change = req.body;
+
+  try {
+    const confirmation = await Potluck.editUserAttendance(id, change);
+    res.status(200).json(confirmation);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
