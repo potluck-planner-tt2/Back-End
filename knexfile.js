95058176119521a57fd9889b15e0a61376069638
Joseph LaSata <61367542+joeywire@users.noreply.github.com>
2021-01-04 12:27:46 -0600

    Revert "ssl config added"

--- knexfile.js
+++ knexfile.js
@@ -28,9 +28,6 @@ module.exports = {
   production: {
     client: 'pg',
     connection: pgConnection,
-    ssl: {
-      rejectUnauthorized: false,
-    },
     pool: {
       min: 2,
       max: 10,
